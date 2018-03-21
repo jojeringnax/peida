8e6c7fee395b9c73e5fbccaecf61ed651a5a4b1d
jojeringnax <hollow718@gmail.com>
2018-03-21 09:14:20 +0300

    QUestions finished

--- resources/views/index/questions.blade.php
+++ resources/views/index/questions.blade.php
@@ -1,19 +1,33 @@
 <section id="questions">
     <div class="flex questions">
-        <div class="ask_a_question">ЗАДАЙТЕ ВОПРОС:</div>
-        <div class="sveta_portrait">
-            <img src="img/questions.png" title="Психология Светлана Пейда Блог" alt="Психология Светлана Пейда Блог" />
+        <div class="flex questions_rules">
+            <div class="ask_a_question">ЗАДАЙТЕ ВОПРОС:</div>
+            <div class="sveta_portrait">
+                <img src="img/questions.png" title="Психология Светлана Пейда Блог" alt="Психология Светлана Пейда Блог" />
+            </div>
+            <div class="rules">
+                <span style="font-size: 12px;" ><p><i>ПРАВИЛА</i></p>
+                    <i>Если Вы хотите писать, то надо:
+                        <br />зачем терпеть проверка связи снова
+                        <br />и теперь никогда не проходит с
+                        <br />возрастом и приходится увергать
+                        <br />эей эейи гей и теперь и заново
+                        <br />верить...
+                    </i>
+                    <br />
+                    <div style="text-align: right;color: #679280">Далее...</div>
+                </span>
+            </div>
         </div>
-        <div class="rules">
-            <span style="font-size: 12px;" ><p><i>ПРАВИЛА</i></p>
-                <i>Если Вы хотите писать, то надо:
-                    <br />зачем терпеть проверка связи снова
-                    <br />и теперь никогда не проходит с
-                    <br />возрастом и приходится увергать
-                    <br />эей эейи гей и теперь и заново
-                    <br />верить...
-                </i>
-            </span>
+        <div class="flex questions_slider">
+            <div class="hidden-xs arrow left" style="display:none;">
+                <img src="img/pic/left.png" />
+            </div>
+            <div class="hidden-lg hidden-md question_window first"></div>
+            <div class="hidden-xs question_window second"></div>
+            <div class="hidden-xs arrow right">
+                <img src="img/pic/right.png" />
+            </div>
         </div>
     </div>
 </section>
